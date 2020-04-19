8fd20eeea024a7d813bebeeea073d9d6b92c0a5d
ILIVE0146 <yogannk@gmail.com>
2020-04-20 00:02:28 +0530

    comments added for easy reading

--- app.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include "menu.hpp"
-int main(){
-    menu title;
-    title.printTitle();
-    int r;
-    cin>>r;
-}
--- menu.hpp
+++ menu.hpp
@@ -4,33 +4,15 @@
 #include<iostream>
 #include <fstream>
 #include <string>
+#include<Windows.h>
 //------------------------------------------------------------------------------------------------------------------
 using namespace std;
-/*class mainMenu{
-    private:
-    string Lines = "";       //All lines
+class Menu{
     public:
-    string getFileContents (ifstream &menu){
-    if (menu)                      //Check if everything is good
-    {
-	    while (menu.good ())
-	    {
-	        string TempLine;                  //Temp line
-	        getline(menu , TempLine);        //Get temp line
-	        TempLine += "\n";                      //Add newline character
-	        Lines += TempLine;                     //Add newline
-	    }
-	    return Lines;
-    }else{                           //Return error
-	    return "ERROR File does not exist.";
-        }
-    }
-};
-*/
-class menu{
-    public:
+    //                this class just prints a title name and other options like start,continue,exit
+    //                TODO : this class can also play background music for the title screen
     void printTitle(){
-        cout << R"(
+      cout << R"(
                                             _                   
            _ __   __ _ _ __ ___   ___      | |__   ___ _ __ ___ 
           | '_ \ / _` | '_ ` _ \ / _ \     | '_ \ / _ \ '__/ _ \
@@ -39,5 +21,38 @@ class menu{
                                                   
 
     )"<<endl;
+    //Sleep(int milliseconds)   --this function puts the console on hold for the given time in milliseconds   --needs to include windows.h
+    Sleep(2000);
+      cout<<R"(
+
+
+            _ |                   |                 |   
+              |              __|  __|   _` |   __|  __| 
+              |            \__ \  |    (   |  |     |   
+             _|     _)     ____/ \__| \__,_| _|    \__| 
+                                                        
+
+      )"<<endl;
+      Sleep(1000);
+      cout<<R"(
+
+        ___ \                          |   _)                     
+           ) |      __|   _ \   __ \   __|  |  __ \   |   |   _ \ 
+          __/      (     (   |  |   |  |    |  |   |  |   |   __/ 
+        _____| _) \___| \___/  _|  _| \__| _| _|  _| \__,_| \___| 
+                                                                  
+
+      )"<<endl;
+      Sleep(1000);
+      cout<<R"(
+
+            ___ /                          _)  |   
+              _ \               _ \ \ \  /  |  __| 
+               ) |              __/  `  <   |  |   
+            ____/      _)     \___|  _/\_\ _| \__| 
+                                                   
+
+      )"<<endl;
+      Sleep(1000);
     }
 };
