e5d59b630642052e3a930f14618ecbcd2da2a542
abishek982001 <abishek982001@gmail.com>
2020-04-26 11:56:47 +0530

    code added for combat

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 To learn c++
    //TODO : fix a name for the game
 SYSTEM REQUIREMENTS : windows only
+
+**This game is not yet fully functional**
+# TODO:
+ * **Add story** 
+ * Include Animation
+ * Some bug fixes 
+ * Background Music
+ * File handling to store game state to enable coninue option
--- main.cpp
+++ main.cpp
@@ -1,14 +1,68 @@
+/*
+Modified by: ABISHEK.R
+Last Modified:25-04-2020
+Modification details: Created a global header and added code for player details and performing comabt in game header.
+TO DO: 
+    1, Add a story script
+    2, Add Animation
+    3, Add Backgroubd Music
+    4, Bug fixes
+    5, File Handling to enable continue option 
+*/ 
 //--------------------INBUILD HEADERS--------------------------------------
 #include<iostream>
+#include<Windows.h>
 //-------------------------------------------------------------------------
 //---------------------USER DEFINED HEADERS---------------------------------
-#include "Menu.hpp"
-#include"State.hpp"
+#include "game.hpp"
 //--------------------------------------------------------------------------
+
 using namespace std;
 int main(){
-    Menu title;       //creating an object called title from class Menu
-    title.printTitle();      //printing title screen
-    State game;  //creating an object called game from class State
-    cin>>game.gameState;  
+    totalHealth=health;
+    maxHealth=totalHealth;
+    system("cls");
+    Game game;       //creating an object called title from class Menu
+    game.printTitle();     //printing title screen
+    x:
+    if(input==1){
+        system("cls");
+        cout<<"Creating character.\n";
+        Sleep(200);
+        system("cls");
+        cout<<"Creating character..\n";
+        Sleep(200);
+        system("cls");
+        cout<<"Creating character...\n";
+        Sleep(200);
+        system("cls");
+        cout<<"Creating character....\n";
+        Sleep(200);
+        system("cls");
+        cout<<"Creating character.....\n";
+        Sleep(200);
+
+
+    //Write the story plot for this game
+
+    
+         game.HUD();
+    }
+    else if(input==2)
+        
+        /*___________________________________________________________________________________________________________
+          code to save the state of the game in a file
+        _____________________________________________________________________________________________________________
+        */
+       cout<<"Sorry!!       This feature is not availabe for now!!";
+    else if(input==3)
+        exit(0);
+    else
+    {
+        cout<<"Invalid input\n";
+        cout<<"Please try again!\n";
+        cin>>input;
+        goto x;
+    }
+    
 }
