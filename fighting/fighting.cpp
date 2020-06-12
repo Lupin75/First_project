6920254e295207a08b966b77e3022cd8efc40d86
ILIVE0146 <yogannk@gmail.com>
2020-06-12 15:57:43 +0530

    added display remaining health
    now if either player or enemy jumps their health will be increased

--- fighting/fighting.cpp
+++ fighting/fighting.cpp
@@ -102,83 +102,109 @@ class Fighting{
         fightFunction::reset();
     }
     public:
-    //startBattle works both as a constructor and as a function.
-    bool startBattle(std::string ename/*enemyName*/,int enemyDefendStat/*in percentage*/=50,int playerBaseDamage=10,int enemyBaseDamage=10,int eH=10,bool isPlayerFirst = true){
+    //startBattle works both as a constructor and as a function.ename is enemy name. eH is enemy health
+    bool startBattle(std::string ename/*enemyName*/,int enemyDefendStat/*in percentage*/=50,int playerBaseDamage=10,int enemyBaseDamage=10,int eH=10,int playerHealInPercentage = 50,int enemyhealInPercentage = 50,bool isPlayerFirst = true){
         int enemyHealth = eH;
         int PBD = playerBaseDamage;
         int EBD = enemyBaseDamage;
         this->enemyName=ename;
+        int enemyMaxHealth = enemyHealth;
         //printing instructions for the battle
-        std::cout<<"\t1:Type `attack` to attack the enemy."<<std::endl<<"\t2:Type `defend` to brace yourself for impact(you will take damage but extremely lower than usual.defending yourself will increase the attack power if you `attack` right after `defend`)"<<std::endl;
-        std::cout<<"\t3:Type `jump` to jump backward to completely miss the enemy attack, however if you type `attack` right after `jump` your damage output will be lower"<<std::endl;
-        Sleep(3000);
+        std::cout<<"INSTRUCTIONS"<<std::endl;
+        std::cout<<"\t1:Type `attack` to attack the enemy."<<std::endl<<"\t2:Type `defend` to brace yourself for impact(you will take damage but 1/4th than usual.defending yourself will increase the attack power if you `attack` right after `defend`)"<<std::endl;
+        std::cout<<"\t3:Type `jump` to jump backward to completely miss the enemy attack and heal yourself, however if you type `attack` right after `jump` your damage output will be lower"<<std::endl;
+        system("pause");
+        std::cout<<"the battle has begun!"<<std::endl;
         if(isPlayerFirst == true){
-            std::cout<<this->enemyName<<" is infront for you"<<std::endl;
+            std::cout<<this->enemyName<<" is infront for you"<<std::endl<<"YOU:"<<std::endl;
             this->getEvent();
             if(fightFunction::playerLogic == "attack" ){
-                std::cout<<"You attacked "<<this->enemyName<<" and inflicted "<<playerBaseDamage*(enemyDefendStat/100)<<std::endl;
+                std::cout<<"You attacked "<<this->enemyName<<" and inflicted "<<playerBaseDamage*(enemyDefendStat/100)<<" damage on "<<this->enemyName<<std::endl;
                 enemyHealth=enemyHealth-(playerBaseDamage*(enemyDefendStat/100));
             }else if(fightFunction::playerLogic=="defend"){
                 std::cout<<"you proceeded with caution and the enemy saw you."<<std::endl;
-            }else if(fightFunction::playerLogic=="jump"){
+            }else if(fightFunction::playerLogic== "jump" ){
                 std::cout<<"you stepped back a bit,the enemy saw you"<<std::endl;
+                playerHealth = playerHealth + (playerHealth*playerHealInPercentage/100);
             }
             else{
                 std::cout<<"You stared at him, both drawed swords and assumed battle positions"<<std::endl;
             }
         }else{
             this->generateEnemyLogic();
-            std::cout<<"the enemy is about to "<<fightFunction::enemyLogic<<"   you: "<<std::endl;
+            std::cout<<"The enemy is about to "<<fightFunction::enemyLogic<<std::endl<<"you:"<<std::endl;
             this->getEvent();
             if(fightFunction::playerLogic == "attack"){
                 fightFunction::playerPreviousLogic=fightFunction::playerLogic;
                 if(fightFunction::enemyLogic=="attack"){
-                    std::cout<<"you inflicted "<< playerBaseDamage*(enemyDefendStat/100) <<std::endl;
-                    std::cout<<this->enemyName<<" infilcted "<< enemyBaseDamage*(playerDefendStat/100)<<std::endl;
+                    std::cout<<"you inflicted "<< playerBaseDamage*(enemyDefendStat/100) <<" damage to "<<this->enemyName<<std::endl;
+                    std::cout<<this->enemyName<<" infilcted "<< enemyBaseDamage*(playerDefendStat/100)<<" damage on you"<<std::endl;
                     playerHealth=playerHealth-(enemyBaseDamage*(playerDefendStat/100));
                     enemyHealth=enemyHealth-(playerBaseDamage*(enemyDefendStat/100));
                     fightFunction::enemyPreviousLogic=fightFunction::enemyLogic;
                 }else if(fightFunction::enemyLogic=="defend"){
                     std::cout<<this->enemyName<<" defended";
-                    std::cout<<"you inflicted "<<(playerBaseDamage*(enemyDefendStat/100))/4;
+                    std::cout<<"you inflicted "<<(playerBaseDamage*(enemyDefendStat/100))/4<<" on the "<<this->enemyName<<std::endl;
                     fightFunction::enemyPreviousLogic=="defend";
                 }else{
                     std::cout<<this->enemyName<<" jumped back, you did no damage."<<std::endl;
-                    fightFunction::enemyPreviousLogic=fightFunction::enemyLogic;
+                    if(enemyHealth<enemyMaxHealth){
+                        fightFunction::enemyPreviousLogic=fightFunction::enemyLogic;
+                    }
                 }
             }else if(fightFunction::playerLogic=="defend"){
                 fightFunction::playerPreviousLogic=fightFunction::playerLogic;
                 std::cout<<"you defended"<<std::endl;
                 std::cout<<"your next attack will be 3/4th stronger!"<<std::endl;
                 if(fightFunction::enemyLogic=="attack"){
-                    std::cout<<this->enemyName<<" inflicted "<<(enemyBaseDamage*(playerDefendStat/100))/4;
+                    std::cout<<this->enemyName<<" inflicted "<<(enemyBaseDamage*(playerDefendStat/100))/4<<" damage on you"<<std::endl;
                     playerHealth = playerHealth - (enemyBaseDamage*(playerDefendStat/100))/4 ;
                 }else if(fightFunction::enemyLogic=="defend"){
                     std::cout<<this->enemyName<<" did nothing"<<std::endl;
                     fightFunction::enemyPreviousLogic=fightFunction::enemyLogic;
                 }else{
                     std::cout<<this->enemyName<<" did nothing"<<std::endl;
-                    fightFunction::enemyPreviousLogic=fightFunction::enemyLogic;
+                    if(enemyHealth<enemyMaxHealth){
+                        fightFunction::enemyPreviousLogic=fightFunction::enemyLogic;
+                    }
                 }
-            }else if(fightFunction::playerLogic=="jump"){
+            }else if(fightFunction::playerLogic == "jump"){
                 std::cout<<"you jumped!"<<std::endl;
+                std::cout<<"you got heled"<<std::endl;
+                playerHealth = playerHealth + (playerHealth*playerHealInPercentage/100);
                 std::cout<<"your next attack will be 1/4th of the original!"<<std::endl;
                 fightFunction::enemyPreviousLogic=fightFunction::enemyLogic;
                 fightFunction::playerPreviousLogic=fightFunction::playerLogic;
+                if(fightFunction::enemyLogic == "jump"){
+                    if(enemyHealth<enemyMaxHealth){
+                        enemyHealth=enemyHealth+(enemyHealth*enemyhealInPercentage/100);
+                    }
+                }
             }else{
                 std::cout<<"you failed to move in time!"<<std::endl;
                 if(fightFunction::enemyLogic=="attack"){
-                    std::cout<<this->enemyName<<" infilcted "<< enemyBaseDamage*(playerDefendStat/100)<<std::endl;
+                    std::cout<<this->enemyName<<" infilcted "<< enemyBaseDamage*(playerDefendStat/100)<<" damage on you"<<std::endl;
                     playerHealth=playerHealth-(enemyBaseDamage*(playerDefendStat/100));
+                }else if(fightFunction::enemyLogic == "jump"){
+                    if(enemyHealth < enemyMaxHealth){
+                        enemyHealth=enemyHealth+(enemyHealth*enemyhealInPercentage/100);
+                    }
                 }
                 fightFunction::playerPreviousLogic = "none";
                 fightFunction::enemyPreviousLogic = fightFunction::enemyLogic;
             }
         }
+        std::cout<<"YOU HEALTH IS "<<playerHealth<<std::endl;
+        std::cout<<this->enemyName<<" HEALTH IS "<<enemyHealth<<std::endl;
+        system("pause");
         while(playerHealth >=0 && enemyHealth >= 0 ) {
+            std::cout<<"YOU HEALTH IS "<<playerHealth<<std::endl;
+            std::cout<<this->enemyName<<" HEALTH IS "<<enemyHealth<<std::endl;
+            system("pause");
             this->generateEnemyLogic();
             //continue battle here
             std::cout<<this->enemyName<<" is about to "<<fightFunction::enemyLogic<<std::endl;
+            std::cout<<"YOU: "<<std::endl;
             this->getEvent();
             if(fightFunction::playerPreviousLogic=="jump"){
                 PBD = PBD/4;
@@ -192,37 +218,49 @@ class Fighting{
             }
             if(fightFunction::playerLogic=="attack"){
                 if(fightFunction::enemyLogic=="attack"){
-                    std::cout<<"You inflicted "<<(PBD*enemyDefendStat/100)<<" on "<<this->enemyName<<std::endl;
-                    std::cout<<this->enemyName<<" inflicted "<<(EBD*playerDefendStat/100)<<" on YOU"<<std::endl;
+                    std::cout<<"You inflicted "<<(PBD*enemyDefendStat/100)<<" damage on "<<this->enemyName<<std::endl;
+                    std::cout<<this->enemyName<<" inflicted "<<(EBD*playerDefendStat/100)<<" damage on YOU"<<std::endl;
                     enemyHealth = enemyHealth - (PBD*enemyDefendStat/100);
                     std::cout<<enemyHealth<<std::endl;
                     playerHealth = playerHealth - (EBD*playerDefendStat/100);
                 }else if(fightFunction::enemyLogic=="defend"){
-                    std::cout<<"you inflicted "<<((PBD*enemyDefendStat/100)/4)<<std::endl;
+                    std::cout<<"you inflicted "<<((PBD*enemyDefendStat/100)/4)<<" damage on "<<this->enemyName<<std::endl;
                     enemyHealth = enemyHealth - ((PBD*enemyDefendStat/100)/4);
                     std::cout<<enemyHealth<<std::endl;
                 }else if(fightFunction::enemyLogic=="jump"){
                     std::cout<<"you did no damage to "<<this->enemyName<<std::endl;
+                    if(enemyHealth<enemyMaxHealth){
+                        enemyHealth = enemyHealth+(enemyHealth*enemyhealInPercentage/100);
+                    }
                 }
                 fightFunction::playerPreviousLogic=fightFunction::playerLogic;
                 fightFunction::enemyPreviousLogic=fightFunction::enemyLogic;
             }else if(fightFunction::playerLogic=="defend"){
+                std::cout<<"you defended"<<std::endl;
                 if(fightFunction::enemyLogic=="attack"){
-                    std::cout<<this->enemyName<<" inflicted "<<(EBD*(playerDefendStat/100))/4<<" on you"<<std::endl;
+                    std::cout<<this->enemyName<<" inflicted "<<(EBD*(playerDefendStat/100))/4<<" damage on you"<<std::endl;
                     playerHealth=playerHealth-(EBD*(playerDefendStat/100))/4;
                 }else if(fightFunction::enemyLogic=="defend"){
                     std::cout<<"both defended, really!"<<std::endl;
                 }else if(fightFunction::enemyLogic=="jump"){
                     std::cout<<"nothing happened, it just felt like 2 magikarp fighting eachother(-_-)"<<std::endl;
+                    if(enemyHealth<enemyMaxHealth){
+                        enemyHealth = enemyHealth+(enemyHealth*enemyhealInPercentage/100);
+                    }
                 }
                 fightFunction::playerPreviousLogic=fightFunction::playerLogic;
                 fightFunction::enemyPreviousLogic=fightFunction::enemyLogic;
             }else if(fightFunction::playerLogic=="jump"){
+                std::cout<<"you jumped"<<std::endl;
+                playerHealth = playerHealth + (playerHealth*playerHealInPercentage/100);
                 if(fightFunction::enemyLogic=="attack"){
                     std::cout<<"you evaded the attack!"<<std::endl;
                 }else if(fightFunction::enemyLogic=="defend"){
                     std::cout<<"nothing happened, it just felt like 2 magikarp fighting eachother(-_-)"<<std::endl;
                 }else if(fightFunction::enemyLogic=="jump"){
+                    if(enemyHealth<enemyMaxHealth){
+                        enemyHealth = enemyHealth + (enemyHealth*enemyhealInPercentage/100);
+                    }
                     std::cout<<this->enemyName<<" and you both finished playing dance floor arcade machine,it was a tie"<<std::endl;
                 }
                 fightFunction::playerPreviousLogic=fightFunction::playerLogic;
@@ -230,8 +268,13 @@ class Fighting{
             }else{
                 if(fightFunction::enemyLogic=="attack"){
                     std::cout<<this->enemyName<<" inflicted "<<EBD*playerDefendStat/100<<" on you while you were staring at him"<<std::endl;
+                }else if(fightFunction::enemyLogic=="defend"){
+                    std::cout<<this->enemyName<<" did nothing, due to your insane luck"<<std::endl;
                 }else{
                     std::cout<<this->enemyName<<" did nothing, due to your insane luck"<<std::endl;
+                    if(enemyHealth<enemyMaxHealth){
+                        enemyHealth=enemyHealth+(enemyHealth*enemyhealInPercentage/100);
+                    }
                 }
                 fightFunction::playerPreviousLogic="NULL";
                 fightFunction::enemyPreviousLogic=fightFunction::enemyLogic;
@@ -247,7 +290,7 @@ class Fighting{
 int main(){
     Fighting f;
     if(!f.startBattle("nirmalie")){
-        std::cout<<"you died"<<std::endl;
+        std::cout<<"you died!"<<std::endl;
     }else{
         std::cout<<"success!"<<std::endl;
     }
