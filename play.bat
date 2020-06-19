408e782ef9d8e686fa5ff37ba57096bdb24172c0
ILIVE0146 <yogannk@gmail.com>
2020-06-19 19:23:51 +0530

    bat file added
    idk if it's a good idea or not but now the game can be played by double clicking play.bat

---
+++ play.bat
@@ -0,0 +1,3 @@
+g++ -std=c++17 story.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network -I"resources/externalDependencies/include" -L"resources/externalDependencies/lib" -o ./BUILD/game
+cd BUILD
+game.exe
